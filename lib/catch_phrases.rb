f7d81abfb7ad025e23279ef3eb75cceb42e9e7dd
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-04 19:06:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,7 +4,7 @@ def mario
 end
 
 def toadstool
-  status = 'Thank you Mario! But our Princess Is In Another Castle!'
+  status = "Thank you Mario! But our Princess Is In Another Castle!"
   puts status
 end
 
